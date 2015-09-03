ac00649ca0ee258678dc483f0553f82d6939cdb7
Nick Troccoli <nicktroccoli28@gmail.com>
2015-09-03 02:55:53 -0400

    Test: changed greeting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.json({"greeting": "Hello world!"});
+  response.json({"greeting": "Hello there!"});
 });
 
 app.listen(app.get('port'), function() {
